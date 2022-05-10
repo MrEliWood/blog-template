240586e3a40c15e52c75c497b20f074c6d898dfa
Eli Wood <mreliwood1@gmail.com>
2022-05-09 23:43:11 -0700

    working application

--- public/js/comments.js
+++ public/js/comments.js
@@ -12,31 +12,39 @@ for (let i = 0; i < commentButton.length; i++) {
 
         event.preventDefault();
 
-        // construct body with comment
-        const commentObject = {
-            body: commentInputs[i].value,
-            blog_id: event.target.value
-        };
+        const loggedIn = document.getElementById('navUsername');
 
-        // post comment request
-        fetch("/comments", {
+        if (loggedIn == null) {
+            window.location.replace("/login");
+        } else {
 
-            method: "POST",
-            body: JSON.stringify(commentObject),
-            headers: {
-                "Content-Type": "application/json"
-            }
+            // construct body with comment
+            const commentObject = {
+                body: commentInputs[i].value,
+                blog_id: event.target.value
+            };
 
-            })
-            .then(res => {
+            // post comment request
+            fetch("/comments", {
 
-                if (res.ok) {
-                    location.reload();
-                } else {
-                    alert("Post error, please try again.");
-                };
+                method: "POST",
+                body: JSON.stringify(commentObject),
+                headers: {
+                    "Content-Type": "application/json"
+                }
 
-            });
+                })
+                .then(res => {
+
+                    if (res.ok) {
+                        location.reload();
+                    } else {
+                        alert("Post error, please try again.");
+                    };
+
+                });
+
+        }
 
     });
 
